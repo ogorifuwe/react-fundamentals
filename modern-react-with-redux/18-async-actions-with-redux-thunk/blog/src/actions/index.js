9c6372431d3ae16ac2128eee0b9c221c6f272e76
oi <oifuwe@luc.edu>
2022-02-15 14:29:16 -0600

    add async actions with redux thunk

---
+++ modern-react-with-redux/18-async-actions-with-redux-thunk/blog/src/actions/index.js
@@ -0,0 +1,10 @@
+ import jsonPlaceholder  from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = jsonPlaceholder.get('/posts');
+
+  dispatch({
+    type: 'FETCH_POSTS',
+    payload: response
+  });
+};
