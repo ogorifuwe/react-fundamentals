f4cf96f778f995355deb7efb14aefbf24faaf8c5
oi <oifuwe@luc.edu>
2022-02-22 13:10:11 -0600

    add context system intro

---
+++ modern-react-with-redux/27-the-context-system-with-react/translate/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a Language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+
+        <ColorContext.Provider value ="primary">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
